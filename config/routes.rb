8c259037daae7124876c275600c4066428b4320c
Qaisar <kayser.abbas@gmail.com>
2024-02-27 17:19:19 +0500

    Add route for admin views

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "home#index", controller: "home"
-
+  
   authenticate :admin do
     root "admin#index", as: :admin_root
   end
-
+  
+  get "admin" => "admin#index"
+  
+  # Defines the root path route ("/")
+  root "home#index"
 end
