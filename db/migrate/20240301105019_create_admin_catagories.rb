c1242e61f5fcf843b7749810e6ee9ba5f38e49aa
Qaisar <kayser.abbas@gmail.com>
2024-03-01 16:46:41 +0500

    Generate migration file for admin categories

---
+++ db/migrate/20240301105019_create_admin_catagories.rb
@@ -0,0 +1,10 @@
+class CreateAdminCatagories < ActiveRecord::Migration[7.1]
+  def change
+    create_table :admin_catagories do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
