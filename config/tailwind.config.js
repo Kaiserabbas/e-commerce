0f7aa6b8d95eea6a3f638e3f0641bdae5afee4da
Qaisar <kayser.abbas@gmail.com>
2024-03-01 16:41:37 +0500

    Add custom color

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      color: {
+        "teal": "#1abc9c",
+        "dark": "#2c3e50",
+      },
     },
   },
   plugins: [
