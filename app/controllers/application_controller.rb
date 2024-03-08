805b2eb74c30af1739eb6f89829b7b24fcc9afef
Qaisar <kayser.abbas@gmail.com>
2024-03-08 21:13:31 +0500

    Update after sign in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
+  def after_sign_in_path_for(resource)
+    admin_path
   end
 end
