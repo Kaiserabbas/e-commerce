3d2690c866b03ba82443e2aab2abaec70321bc0a
Qaisar <kayser.abbas@gmail.com>
2024-03-16 21:26:10 +0500

    Update importmsp.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js"
+pin "chart.js" # @4.4.2
 pin "chartkick", to: "https://cdn.jsdelivr.net/npm/chartkick@4.1.3/chartkick.js"
 pin "moment", to: "https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.js"
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
