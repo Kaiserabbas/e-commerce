83cfeed339e25b322b0ff5abdee3197d4ab67c45
Qaisar <kayser.abbas@gmail.com>
2024-04-02 13:47:55 +0500

    Update methods (logic)

--- app/controllers/admin/stocks_controller.rb
+++ app/controllers/admin/stocks_controller.rb
@@ -1,5 +1,5 @@
 class Admin::StocksController < AdminController
-  before_action :set_admin_stock, only: %i[ show edit update destroy ]
+  before_action :set_admin_stock, only: %i[show edit update destroy]
 
   # GET /stocks or /stocks.json
   def index
@@ -30,7 +30,9 @@ class Admin::StocksController < AdminController
 
     respond_to do |format|
       if @admin_stock.save
-        format.html { redirect_to admin_product_stock_url(@product, @admin_stock), notice: "Stock was successfully created." }
+        format.html {
+          redirect_to admin_product_stock_url(@product, @admin_stock), notice: "Stock was successfully created."
+        }
         format.json { render :show, status: :created, location: @admin_stock }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -43,7 +45,10 @@ class Admin::StocksController < AdminController
   def update
     respond_to do |format|
       if @admin_stock.update(admin_stock_params)
-        format.html { redirect_to admin_product_stock_url(@admin_stock.product, @admin_stock ), notice: "Stock was successfully updated." }
+        format.html {
+          redirect_to admin_product_stock_url(@admin_stock.product, @admin_stock),
+                      notice: "Stock was successfully updated."
+        }
         format.json { render :show, status: :ok, location: @admin_stock }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -57,19 +62,22 @@ class Admin::StocksController < AdminController
     @admin_stock.destroy!
 
     respond_to do |format|
-      format.html { redirect_to admin_product_stocks_url(@admin_stock.product), notice: "Stock was successfully destroyed." }
+      format.html {
+        redirect_to admin_product_stocks_url(@admin_stock.product), notice: "Stock was successfully destroyed."
+      }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_admin_stock
-      @admin_stock = Stock.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def admin_stock_params
-      params.require(:stock).permit(:size, :amount)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_admin_stock
+    @admin_stock = Stock.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def admin_stock_params
+    params.require(:stock).permit(:size, :amount)
+  end
 end
