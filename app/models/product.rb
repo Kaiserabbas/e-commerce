7915bb3b5e8a4264987c2e1a4f7e0ecdd0d9627f
Qaisar <kayser.abbas@gmail.com>
2024-04-08 21:58:39 +0500

    Update model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [50, 50]
+    attachable.variant :medium, resize_to_limit: [250, 250]
+  end
+
   belongs_to :catagory
   has_many :stocks
   has_many :order_products
-  has_many_attached :images
 end
